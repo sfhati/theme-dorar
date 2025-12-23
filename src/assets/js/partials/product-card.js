f602b4e20f869a00a18e9536a93d50f25a402a3e
sfhati <sfhati.com@gmail.com>
2025-12-23 05:02:27 +0300

    price

--- src/assets/js/partials/product-card.js
+++ src/assets/js/partials/product-card.js
@@ -231,7 +231,7 @@ class ProductCard extends HTMLElement {
  
           <div class="s-product-card-content-main ${this.isSpecial ? 's-product-card-content-extra-padding' : ''}">
             <h3 class="s-product-card-content-title nnn">
-              <a href="${this.product?.url}">${(this.product?.name || '').split('-').join('<br>')}</a>
+              <a href="${this.product?.url}">${(this.product?.name || '').split('-').join('<br><br>')}</a>
             </h3>
 
             ${this.product?.subtitle && !this.minimal ?
@@ -257,7 +257,7 @@ class ProductCard extends HTMLElement {
           </div>`
             : ''}
           <div class="s-product-card-content-sub ${this.isSpecial ? 's-product-card-content-extra-padding' : ''}">
-            ${this.product?.donation?.can_donate ? '' : this.getProductPrice()}
+            ${this.product?.donation?.can_donate ? '' : this.getProductPrice()} <br>
             ${this.product?.rating?.stars ?
               `<div class="s-product-card-rating">
                 <i class="sicon-star2 before:text-orange-300"></i>
